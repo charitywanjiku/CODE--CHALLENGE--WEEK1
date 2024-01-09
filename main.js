719804964eac5664a757c44300e0cf1099fd2463
charitywanjiku <charity.wanjiku1@student.moringaschool.com>
2024-01-09 17:42:41 +0300

    Initial Commit

--- main.js
+++ main.js
@@ -1,28 +1,5 @@
 
-/*let prompt=require('prompt-sync')()
-//This function helps you to calculate grade of each student.
-function calculateGrade(){
-    
-    var marks=parseFloat(prompt("Enter student marks:"));//prompts the student to enter their marks
-    if (isNaN(marks) || marks < 0 || marks > 100) {
-        console.log("Invalid input!");
-    }
-     
-    else if(marks>79){
-        return "A";
-    }else if(marks>=60){
-        return "B";
-    }else if(marks>=50){
-        return "C";
-    }else if(marks>=40){
-        return "D";
-    }else{
-        return "E";
-    }
-}
 
-var grade =calculateGrade()//calls the function
-console.log("Grade:",grade);//prints out grade of the student*/
 let prompt = require("prompt-sync")();
 
 //Function that Calculates and displays the student's grade.
